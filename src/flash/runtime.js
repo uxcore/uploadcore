4008579e9621f63ac9525b101fe6504bd3a661c2
yanbingbing <me@yanbingbing.com>
2015-09-28 15:27:02 +0800

    use native appendChild

--- src/flash/runtime.js
+++ src/flash/runtime.js
@@ -73,13 +73,13 @@ export default class FlashRuntime extends Runtime {
 
         const _this = this;
         function display() {
-            let w = trigger[0].offsetWidth, h = trigger[0].offsetHeight, flash;
+            let w = trigger.offsetWidth, h = trigger.offsetHeight, flash;
             if (!w || !h || !(flash = createFlash(swf, callInterface))) {
                 setTimeout(display, 1000);
                 return;
             }
 
-            trigger.append(_this.flash = flash);
+            trigger.appendChild(_this.flash = flash);
         }
 
         display();

555ac50e2b76b34e76fce55f6b338b4157d12664
yanbingbing <me@yanbingbing.com>
2015-12-29 14:42:18 +0800

    version 2.2.1

--- dist/uploadcore.js
+++ dist/uploadcore.js
@@ -66,7 +66,7 @@ return /******/ (function(modules) { // webpackBootstrap
 	Core.Events = Events;
 	Core.Status = Status;
 	Core.UploadCore = Core;
-	Core.VERSION = ("2.2.0");
+	Core.VERSION = ("2.2.1");
 	Core.Core = Core;
 	
 	module.exports = Core;

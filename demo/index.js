fa3bfd5f1661b13ef3849fd27ca2e0572dce1539
yanbingbing <me@yanbingbing.com>
2018-12-14 15:43:09 +0800

    refactor

--- demo/index.js
+++ demo/index.js
@@ -1,33 +1,65 @@
-const {Core, Events, Status} = require('uploadcore');
-
-Core.setSWF('/dist/flashpicker.swf');
+const { Core, Events, Status } = require('uploadcore');
 
 const core = new Core({
-    request: {
-        name: 'file',
-        url: 'http://test.yanbingbing.com/upload.php',
-        params: {},
-        headers: null,
-        withCredentials: false,
-        timeout: 0,
-        chunkEnable: false
-    },
+    name: 'file',
+    url: 'http://test.yanbingbing.com/upload.php',
+    params: {},
+    headers: null,
+    withCredentials: false,
+    timeout: 0,
+    chunkEnable: true,
+    chunkSize: '2m',
+    chunkProcessThreads: 3,
     processThreads: 3,
     autoPending: true,
     queueCapcity: 0,
     multiple: true,
     accept: null,
-    sizeLimit: '1g',
+    sizeLimit: '2g',
     preventDuplicate: false
 });
 
 window.core = core;
-core.setOptions({
-    queueCapcity: 2
-});
 
-// 上传完成
-core.on(Events.FILE_UPLOAD_COMPLETING, response => {
+
+let token = null;
+core.on(Events.FILE_UPLOAD_PREPARING, (request) => {
+    if (token) {
+        request.setParam('token', token);
+    } else {
+        return fetch('http://test.yanbingbing.com/token.php').then((res) => {
+            return res.json();
+        }).then((json) => {
+            token = json.token;
+            request.setParam('token', token);
+        });
+    }
+}).on(Events.FILE_UPLOAD_PREPARING, (request) => {
+    if (request.isChunkEnable()) {
+        const size = request.file.size;
+        const ext = request.file.ext;
+        const chunkTotal = Math.ceil(size / request.getChunkSize());
+        return fetch(`http://test.yanbingbing.com/transaction.php?size=${size}&ext=${ext}&chunkTotal=${chunkTotal}`).then((res) => {
+            return res.json();
+        }).then((json) => {
+            request.setParam('transId', json.data.transId);
+        });
+    }
+}).on(Events.CHUNK_UPLOAD_PREPARING, (request) => {
+    if (request.isMultiChunk()) {
+        request.setUrl('http://test.yanbingbing.com/chunk.php');
+        request.setParam('sequence', request.getIndex());
+    }
+}).on(Events.FILE_UPLOAD_COMPLETING, (response) => {
+    if (response.isFromMultiChunkResponse()) {
+        const transId = response.getFileRequest().getParam('transId');
+        return fetch(`http://test.yanbingbing.com/transaction.php?transId=${transId}`).then(res => {
+            return res.json();
+        }).then(json => {
+            response.setResponse(json);
+        });
+    }
+}).on(Events.FILE_UPLOAD_COMPLETING, response => {
     let json = response.getJson();
     if (!json || json.code !== 0) {
         return new Error('error');
@@ -44,7 +76,7 @@ core.on(Events.QUEUE_FILE_FILTERED, (file, error) => console.info('queuefilefilt
 core.on(Events.QUEUE_FILE_ADDED, (file) => {
     console.info('queuefileadded', file);
     file.session().progress((e) => {
-        console.info('progress', e);
+        console.info('progress', e.percentage);
     }).fail((error) => {
         console.info(error)
     }).done((response) => {
@@ -53,43 +85,43 @@ core.on(Events.QUEUE_FILE_ADDED, (file) => {
 });
 
 core.on(Events.QUEUE_UPLOAD_START, () => {
-   console.info('queueuploadstart');
+   // console.info('queueuploadstart');
 });
 
 core.on(Events.QUEUE_UPLOAD_END, () => {
-    console.info('queueuploadend');
+    // console.info('queueuploadend');
 });
 
 core.on(Events.QUEUE_STAT_CHANGE, (stat) => {
-    console.info('statchange', stat.stat());
+    // console.info('statchange', stat.stat());
 });
 
 core.on(Events.FILE_UPLOAD_START, (file) => {
-    console.info('fileuploadstart', file);
+    // console.info('fileuploadstart');
 }).on(Events.FILE_UPLOAD_PREPARING, (request) => {
-    console.info('fileuploadpreparing', request);
+    // console.info('fileuploadpreparing');
 }).on(Events.FILE_UPLOAD_PREPARED, (file) => {
-    console.info('fileuploadprepared', file);
+    // console.info('fileuploadprepared');
 }).on(Events.CHUNK_UPLOAD_PREPARING, (request) => {
-    console.info('chunkuploadpreparing', request);
+    console.info('chunkuploadpreparing', request.getIndex());
 }).on(Events.CHUNK_UPLOAD_COMPLETING, (response) => {
-    console.info('chunkuploadcompleting', response);
+    console.info('chunkuploadcompleting', response.getChunkRequest().getIndex());
 }).on(Events.FILE_UPLOAD_PROGRESS, (file, progress) => {
-    console.info('fileuploadprogress', file, progress);
+    // console.info('fileuploadprogress', progress);
 }).on(Events.FILE_UPLOAD_END, (file) => {
-    console.info('fileuploadend', file);
+    // console.info('fileuploadend');
 }).on(Events.FILE_UPLOAD_COMPLETING, (response) => {
-    console.info('fileuploadcompleting', response);
+    // console.info('fileuploadcompleting');
 }).on(Events.FILE_UPLOAD_SUCCESS, (file, response) => {
-    console.info('fileuploadsuccess', file, response);
+    // console.info('fileuploadsuccess');
 }).on(Events.FILE_UPLOAD_ERROR, (file, error) => {
-    console.info('fileuploaderror', file, error);
+    // console.info('fileuploaderror', error);
 }).on(Events.FILE_UPLOAD_COMPLETED, (file, status) => {
-    console.info('fileuploadcompleted', file, status);
+    // console.info('fileuploadcompleted', status);
 }).on(Events.FILE_CANCEL, (file) => {
-    console.info('filecancel', file);
+    // console.info('filecancel');
 }).on(Events.FILE_STATUS_CHANGE, (file, status) => {
-    console.info('filestatuschange', file, status, file.getStatusName());
+    // console.info('filestatuschange', status, file.getStatusName());
 });
 
 /*
--- src/collector/picker.js
+++ src/collector/picker.js
@@ -1,17 +1,13 @@
 const Emitter = require('../emitter');
 const Html5Runtime = require('../html5/runtime');
-const FlashRuntime = require('../flash/runtime');
 const File = require('../file');
-const {extend} = require('../util');
-
-
-let SWF_URL = '';
+const { extend } = require('../util');
 
 const createElement = (() => {
     if (typeof document !== 'object') {
         return;
     }
-    
+
     var div = document.createElement('div');
     return function (html) {
         div.innerHTML = html;
@@ -21,96 +17,26 @@ const createElement = (() => {
     };
 })();
 
-class FlashTriggerCollection {
+class Html5TriggerCollection {
     constructor(core, onFiles) {
-        const overlay = createElement('<label style="position:fixed;left:-100px;top:-100px;width:50px;height:50px;display:block;cursor:pointer;overflow:hidden;z-index:99999;opacity:0;filter:alpha(opacity=0)"></label>');
-
-        const runtime = new FlashRuntime(overlay, SWF_URL, () => {
-            return {
-                accept: core.getAccept(),
-                multiple: core.isMultiple()
-            };
-        });
-
-        runtime.on('select', (e) => {
-            onFiles(e.files, runtime);
-            this.current && this.current.emit('files', e.files, runtime);
-        });
-
-        runtime.on('rollOut', () => this.hideOverlay());
-
-        document.body.appendChild(overlay);
-        this.overlay = overlay;
-    }
-
-    hideOverlay() {
-        extend(this.overlay.style, {
-            left: '-100px',
-            top: '-100px',
-            width: '50px',
-            height: '50px'
-        });
-        if (this.current) {
-            this.current.emit('rollOut');
-            this.current = null;
-        }
-    }
-
-
-    add(trigger) {
-        const overlay = this.overlay, emitter = new Emitter;
-        const mouseover = () => {
-            const rect = trigger.getBoundingClientRect();
-            extend(overlay.style, {
-                left: rect.left + 'px',
-                top: rect.top + 'px',
-                width: (rect.right - rect.left) + 'px',
-                height: (rect.bottom - rect.top) + 'px'
-            });
-            emitter.emit('rollOver');
-            if (this.current && this.current !== emitter) {
-                this.current.emit('rollOut');
-            }
-            this.current = emitter;
-        };
-
-        if (trigger.addEventListener) {
-            trigger.addEventListener('mouseover', mouseover, false);
-        } else if (trigger.attachEvent) {
-            trigger.attachEvent("onmouseover", mouseover);
-        }
-
-        emitter.destroy = () => {
-            if (this.current === emitter) {
-                this.hideOverlay();
-            }
-            emitter.removeAllListeners();
-            if (trigger.removeEventListener) {
-                trigger.removeEventListener('mouseover', mouseover, false);
-            } else if (trigger.detachEvent) {
-                trigger.detachEvent("onmouseover", mouseover);
-            }
-        };
-
-        return emitter;
-    }
-}
-
-class Html5Trigger extends Emitter {
-    constructor(trigger, core, onFiles) {
-        super();
-
-        this.trigger = trigger;
+        const runtime = Html5Runtime.getInstance();
         this.core = core;
-        this.label = createElement('<label style="position:absolute;top:0;left:0;width:100%;height:100%;display:inline-block;cursor:pointer;background:#fff;overflow:hidden;opacity:0"></label>');
-
+        this.onFiles = (files) => {
+            onFiles(files, runtime);
+        };
         this.onChange = (e) => {
-            onFiles(e.target.files);
+            this.onFiles(e.target.files);
             this.destroyInput();
             this.createInput();
         };
-        trigger.appendChild(this.label);
         this.createInput();
+    }
+
+    destroyInput() {
+        if (!this.input) return;
+        this.input.removeEventListener('change', this.onChange, false);
+        document.body.removeChild(this.input);
+        this.input = null;
     }
 
     createInput() {
@@ -129,45 +55,29 @@ class Html5Trigger extends Emitter {
         }
 
         input.addEventListener('change', this.onChange, false);
-
-        this.label.appendChild(input);
+        document.body.appendChild(input);
         this.input = input;
     }
 
-    destroyInput() {
-        if (!this.input) return;
-        this.input.removeEventListener('change', this.onChange, false);
-        this.label.removeChild(this.input);
-        this.input = null;
-    }
-
-    destroy() {
-        this.destroyInput();
-        this.removeAllListeners();
-        this.trigger.removeChild(this.label);
-    }
-}
-
-class Html5TriggerCollection {
-    constructor(core, onFiles) {
-        const runtime = Html5Runtime.getInstance();
-        this.core = core;
-        this.onFiles = (files) => {
-            onFiles(files, runtime);
-        };
-    }
-
     add(trigger) {
-        return new Html5Trigger(trigger, this.core, this.onFiles);
+        const fn = () => {
+            this.input.click();
+        };
+        trigger.addEventListener('click', fn);
+
+        return {
+            destroy() {
+                trigger.removeEventListener('click', fn);
+            },
+        };
     }
 }
 
 class PickerCollector {
     static setSWF(url) {
-        SWF_URL = url;
+        // empty
     }
     constructor(core) {
-
         const onFiles = (files, runtime) => {
             for (let i = 0, l = files.length; i < l; i++) {
                 if (core.add(new File(runtime, files[i])) < 0) {
@@ -176,11 +86,7 @@ class PickerCollector {
             }
         };
 
-        if (window.File && window.FileList && window.FileReader) {
-            this.triggerCollection = new Html5TriggerCollection(core, onFiles);
-        } else {
-            this.triggerCollection = new FlashTriggerCollection(core, onFiles);
-        }
+        this.triggerCollection = new Html5TriggerCollection(core, onFiles);
     }
 
     addArea(area) {
